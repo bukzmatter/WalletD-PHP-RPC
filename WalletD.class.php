72533c575ebb68ccf9d2ba2193e280afd014a34d
BUKZ <40646621+bukzmatter@users.noreply.github.com>
2018-10-07 18:44:00 -0400

    Update WalletD.class.php

--- WalletD.class.php
+++ WalletD.class.php
@@ -1,102 +1,72 @@
 <?php
-class WalletD {
-
-	//Server Details
-	private $rpcHost = "http://127.0.0.1";
-	private $rpcPort = "1987";
-
-	//RPC Credentials
-	private $rpcUser = "rpcUser";
-	private $rpcPassword = "rpcPassword";
-
-
-public function __construct(/*$rpcHost, $rpcPort, $rpcUser="", $rpcPassword=""*/) {
-		/*$this->rpcHost = $rpcHost;
-		$this->rpcPort = $rpcPort;
-		$this->rpcUser = $rpcUser;
-		$this->rpcPassword = $rpcPassword;*/
+class walletD {
+	private $username;
+	private $password;
+	private $server;
+	private $port;
+	
+	public function __construct($username,$password,$server,$port){
+		$this->username=$username;
+		$this->password=$password;
+		$this->server=$server;
+		$this->port=$port;
 	}
-
-	public function call($query) {
-		$jsonData = json_encode($query);
-		$curlHandle = curl_init($this->rpcHost.":".$this->rpcPort."/json_rpc");
-		curl_setopt($curlHandle, CURLOPT_USERPWD, $this->rpcUser.":".$this->rpcPassword);
-		curl_setopt($curlHandle, CURLOPT_CUSTOMREQUEST, "POST");
-		curl_setopt($curlHandle, CURLOPT_POSTFIELDS, $jsonData);
-		curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curlHandle, CURLOPT_HTTPHEADER, array(
-			'Content-Type: application/json',
-			'Content-Length: '.strlen($jsonData))
-		);
-		$response = json_decode(curl_exec($curlHandle));
-		return $response;
-		curl_close($curlHandle);
-	}
-
-	/********************************************************************
-	public function createAddress();
-	@RETURNS:
-	stdClass Object
-	(
-    [jsonrpc] => 2.0
-    [result] => stdClass Object
-        (
-            [address] => bukzW7b1dss9NMpkdvFR3kRR4CVSBpnVp1vZHmBSSZF...
-        )
-
-	)*********************************************************************/
-	public function createAddress() {
-		$query = array("method" => "createAddress");
-		return $this->call($request);
-	}
-
-/*************************************************************************
-public function getBalance($address='')
-stdClass Object
-(
-    [jsonrpc] => 2.0
-    [result] => stdClass Object
-        (
-            [availableBalance] => 0
-            [lockedAmount] => 0
-        )
-
-)
-**************************************************************************/
-	public function getBalance($address="") {
-		$query = array(
-			"method" => "getBalance",
-				"params" => array("address"=> $address));
-			return $this->call($query);
-	}
-
-/***************************************************************************
-public function getAddresses()
-stdClass Object
-(
-    [jsonrpc] => 2.0
-    [result] => stdClass Object
-        (
-            [addresses] => Array
-                (
-                    [0] => bukzkHduvD15cu26KjE41zC181CNvCJmuDcY...
-                    [1] => bukzZUC8Qmb33xcj4yq81XfZ5Pfmc9a9bWNe...
-                )
-
-        )
-
-)
-******************************************************************************/
-	public function getAddresses() {
-		$query = array("method" => "getAddresses");
-		return $this->call($query);
-	}
-
-
-
-
+  
+  public function call($query){
+    $json = json_encode($query);
+    $ch = curl_init($this->server.":".$this->port."/json_rpc");
+    curl_setopt($ch,CURLOPT_USERPWD,$this->username.":".$this->password);
+    curl_setopt($ch,CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch,CURLOPT_POSTFIELDS, $json);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch,CURLOPT_HTTPHEADER, array(
+		'Content-Type: application/json',
+		'Content-Length: '.strlen($json))
+    );
+    $result = curl_exec($ch);
+    if (!$result) { return curl_errno($ch); }
+    return json_decode($result);
+  }
+  
+  
+  public function getStatus() {
+    $query = array("method" => "getStatus");
+    return $this->call($query);
+  }
+  
+  public function getViewKey() {
+    $query = array("method" => "getViewKey");
+    return $this->call($query);
+  }
+  
+  public function getSpendKeys($address) {
+    $query = array("method" => "getSpendKeys", "params" => array("address"=>$address));
+    return $this->call($query);
+  }
+  
+  public function getAddresses() {
+    $query = array("method" => "getAddresses");
+    return $this->call($query);
+  }
+  
+  public function createAddress() {
+    $query = array("method" => "createAddress");
+    return $this->call($query);
+  }
+  
+  public function deleteAddress($address) {
+    $query = array("method" => "deleteAddress", "params" => array("address"=>$address));
+    return $this->call($query);
+  }
+  
+  public function getBalance($address) {
+    $query = array("method"=>"getBalance", "params"=>array("address"=>$address));
+    return $this->call($query);
+  }
+  
+  public function getBlockHashes($firstBlockIndex, $blockCount) {
+    $query = array("method"=>"getBlockHashes","params"=>array("firstBlockIndex"=>$firstBlockIndex,"blockCount"=>$blockCount));
+    return $this->call($query);
+  }
 }
-
-$w = new WalletD();
-print_r($w->getAddresses());
 ?>
